5933286a008b29328d9887a125b6f3189f4586be
SamanthaStroud <samantha.stroud1@keyin.com>
2025-12-11 06:13:24 -0330

    started the Checkout page 40% done

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
 import React from "react";
-import { lazy, Suspense } from "react";
-
-// import { useState, useEffect } from "react";
 import { Route, Switch } from "wouter";
-
-//import { candyProducts } from "./catalog-page/catalogpage";
 import "./App.css";
 
 //import the header and footer
 import Header from "./components/header";
 import Footer from "./components/footer";
-
-//import the main page info
+//import the pages
 import Mainpage from "./catalog-page/catalogpage";
-// import Checkout from "./checkout-page/checkoutpage.jsx"
+import Checkout from "./checkout-page/checkout";
 import Cartpage from "./cart-page/cart";
 
-//my other page to be added and routed later -sammie
-
-const Cart = lazy(() => import("./catalog-page/catalogpage"));
-
 function App() {
-  //Main App contents (ive adding routing to my catalog page) - sammie
+  //Main App contents
   return (
     <div>
       <Header />
@@ -29,8 +19,7 @@ function App() {
         <Switch>
           <Route path="/" component={Mainpage} />
           <Route path="/cart" component={Cartpage} />
-          {/* Other routes */}
-          {/*<Route path="/Checkout" element={<Checkout />} /> */}
+          <Route path="/checkout" component={Checkout} />
         </Switch>
       </main>
       <Footer />
--- src/checkout-page/checkout.jsx
+++ src/checkout-page/checkout.jsx
@@ -1,1 +1,133 @@
+// Page code by Samantha Stroud
+
 import React from "react";
+import "./checkout.css";
+import { Button, TextInput } from "@mantine/core";
+
+function Checkout() {
+  return (
+    <div className="mainpagebox">
+      <div className="checkoutbox">
+        <div className="checkoutname">
+          <p>Checkout</p>
+        </div>
+        <div className="maincheckformbox">
+          <div className="infobox">
+            <div className="checksecnames">
+              <p>Customer Info:</p>
+              <div className="inputboxs">
+                <TextInput
+                  radius="xl"
+                  placeholder="Last Name"
+                  color="rgba(251, 164, 181, 1)"
+                />
+                <TextInput
+                  pt={"1rem"}
+                  radius="xl"
+                  placeholder="Full Name"
+                  color="rgba(251, 164, 181, 1)"
+                />
+                <TextInput
+                  pt={"1rem"}
+                  radius="xl"
+                  placeholder="Phone Number"
+                  color="rgba(251, 164, 181, 1)"
+                />
+                <TextInput
+                  pt={"1rem"}
+                  c={"#FBA4B5"}
+                  radius="xl"
+                  placeholder="Email"
+                />
+              </div>
+            </div>
+            <div className="checksecnames">
+              <p>Shipping Address</p>
+            </div>
+            <div className="inputboxs">
+              <TextInput
+                radius="xl"
+                placeholder="City"
+                color="rgba(251, 164, 181, 1)"
+              />
+              <TextInput
+                pt={"1rem"}
+                radius="xl"
+                placeholder="Street Address"
+                color="rgba(251, 164, 181, 1)"
+              />
+              <TextInput
+                pt={"1rem"}
+                radius="xl"
+                placeholder="Zip Code"
+                color="rgba(251, 164, 181, 1)"
+              />
+            </div>
+            <div className="checksecnames">
+              <p>Payment</p>
+            </div>
+
+            <div className="inputboxs">
+              <TextInput
+                pt={"1rem"}
+                radius="xl"
+                placeholder="Card Number"
+                color="rgba(251, 164, 181, 1)"
+              />
+              <TextInput
+                pt={"1rem"}
+                radius="xl"
+                placeholder="Cardholder Name"
+                color="rgba(251, 164, 181, 1)"
+              />
+              <TextInput
+                pt={"1rem"}
+                radius="xl"
+                placeholder="Expire Date"
+                color="rgba(251, 164, 181, 1)"
+              />
+              <TextInput
+                pt={"1rem"}
+                radius="xl"
+                placeholder="CVV Number"
+                color="rgba(251, 164, 181, 1)"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="cartandtotalbox">
+        <div className="cartinfobox">
+          {/* add the cart code from cart.jsx */}
+        </div>
+        <div className="subtotalbox">
+          <div className="colourbox">
+            <div className="orderbox">
+              <div className="orderboxname">
+                <h1>Order Summary</h1>
+              </div>
+              <div className="subtotal">
+                <p>Subtotal</p>
+                {/* add code to display the order subtotal */}
+              </div>
+              <hr />
+              <div className="shipping">
+                <p>Shipping</p>
+                {/* add code to display the Shipping ammount */}
+              </div>
+              <hr />
+              <div className="total">
+                <p>Total</p>
+                {/* add code to display the Total ammount */}
+              </div>
+              <Button className="orderbtn" radius={"xl"}>
+                Place Order
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Checkout;
